04291a65e2a0b319c2338a09c6706b3aa6614997
Cfbfamily <highflyeradultedu@gmail.com>
2026-01-01 15:48:27 +0100

    Create firebase-config.js
    Updated

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcmDA_3r7tlzWJwYSOGuSntwAQRNgs95c",
+  authDomain: "cfbf-portal.firebaseapp.com",
+  projectId: "cfbf-portal",
+  storageBucket: "cfbf-portal.firebasestorage.app",
+  messagingSenderId: "12430059424",
+  appId: "1:12430059424:web:c5ef499e3d51fa6dff66c5"
+};
+
+// Initialize Firebase (old compat version for easy use)
+firebase.initializeApp(firebaseConfig);
